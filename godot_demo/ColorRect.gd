a3f5718a442ed43d144deebdf861d5955d089518
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-08-13 13:34:01 -0700

    Better math. WIP.

--- godot_demo/ColorRect.gd
+++ godot_demo/ColorRect.gd
@@ -6,13 +6,16 @@ extends ColorRect
 func _ready():	
 	var tex : TextureLayered = ResourceLoader.load(quilt)
 	var images = []
-	const layer_count = 45
-	for layer_i in range(layer_count):
-		images.push_front(tex.get_layer_data(layer_i))
-	
+	var layer_count = 45
+	var tiles_v = 5
+	var tiles_h = 9
+	var count = 0
+	for j in range(tiles_v, 0, -1):
+		for i in range(tiles_h):
+			var img = tex.get_layer_data(i + j * tiles_v)
+			images.push_back(img)
 	var tex_final : Texture2DArray = Texture2DArray.new()
 	tex_final.create_from_images(images)
-
-	material.set_shader_param("screen", tex)
+	material.set_shader_param("screen", tex_final)
 
 
