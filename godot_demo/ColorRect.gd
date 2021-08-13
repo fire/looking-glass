d45672ad0c4974cc5c0192fd503f867b16b451fe
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-08-13 12:11:39 -0700

    Flip a different way.

--- godot_demo/ColorRect.gd
+++ godot_demo/ColorRect.gd
@@ -5,6 +5,14 @@ extends ColorRect
 	
 func _ready():	
 	var tex : TextureLayered = ResourceLoader.load(quilt)
+	var images = []
+	const layer_count = 45
+	for layer_i in range(layer_count):
+		images.push_front(tex.get_layer_data(layer_i))
+	
+	var tex_final : Texture2DArray = Texture2DArray.new()
+	tex_final.create_from_images(images)
+
 	material.set_shader_param("screen", tex)
 
 
