940b4df3032e4f7a9e8fc42118c1fae6d4dd8bf9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-08-20 21:32:56 -0700

    Got camera.

--- godot_demo/ColorRect.gd
+++ godot_demo/ColorRect.gd
@@ -7,29 +7,35 @@ func _ready():
 	var num_cols = 5
 	var num_rows = 9
 	var total_views = num_cols * num_rows
+	var tex_width : int = 4096
+	var tex_height : int = 4096
+	var view_width : float = float(tex_width) / num_cols
+	var view_height : float = float(tex_height) / num_rows
+	print(view_width)
+	print(view_height)
 	for i in total_views:
+		var viewport = SubViewport.new()
+		add_child(viewport)
+		viewport.owner = owner
 		var camera = Camera3D.new()
-		add_child(camera)
+		viewport.add_child(camera)
 		camera.owner = owner
 		camera.current = true
-		var viewport = rs.viewport_create();
-		rs.viewport_attach_camera(viewport, camera);
-		rs.viewport_set_active(viewport, true);
-		var tex_width : int = 4096
-		var tex_height : int = 4096
-		var view_width : float = float(tex_width) / num_cols
-		var view_height : float = float(tex_height) / num_rows
-		rs.viewport_set_size(viewport, int(view_width), int(view_height))
-		rs.viewport_set_update_mode(viewport, rs.VIEWPORT_UPDATE_ALWAYS)
-		rs.viewport_set_scenario(viewport, get_world_3d().get_scenario())
+		rs.viewport_attach_camera(viewport.get_viewport_rid(), camera.get_camera_rid());
+		rs.viewport_set_active(viewport.get_viewport_rid(), true);
+		rs.viewport_set_size(viewport.get_viewport_rid(), int(view_width), int(view_height))
+		rs.viewport_set_update_mode(viewport.get_viewport_rid(), rs.VIEWPORT_UPDATE_ALWAYS)
+		rs.viewport_set_scenario(viewport.get_viewport_rid(), get_world_3d().get_scenario())
 		rs.camera_set_environment(camera.get_camera_rid(), get_node("WorldEnvironment").get_environment())
-		viewports.push_back(viewport);
+		viewports.push_back(viewport.get_viewport_rid())
 
 func _on_Timer_timeout():
 	var texture_array : Texture2DArray = Texture2DArray.new()
 	var texture_images : Array = []
 	for viewport in viewports:
 		var tex : RID = rs.viewport_get_texture(viewport)
+		if tex == RID():
+			continue
 		var img : Image = rs.texture_2d_get(tex)
 		print(img.get_size())
 		texture_images.push_back(img)
